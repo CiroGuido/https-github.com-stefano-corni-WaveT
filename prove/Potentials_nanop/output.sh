3a4b9a3f0f6b2604952d13ff87fc96ae4b44272c
Silvio Pipolo <silvio.pipolo@gmail.com>
2016-09-30 19:20:29 +0200

    folder "prove" deleted, now tests are in the "tests" folder.

--- prove/Potentials_nanop/output.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-file=$1
-sts=$2
-let ste=sts
-let sts=sts+1
-grep "States  " $file | awk 'NR<='$sts'*('$sts'+1)/2' > ci_mut.inp
-awk '/Root      1 :/{for(i=1;i<='$sts';i++){print $0; getline}}' $file | head -n $ste > ci_energy.inp
-

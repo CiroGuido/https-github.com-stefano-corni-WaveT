b6b896bf5d6ed794ae8a5467fe920c493c7d0033
Stefano Corni <stefano.corni@nano.cnr.it>
2016-11-08 09:47:06 +0100

    Added the missing main_tdplas.f90 to previous commit

---
+++ main_tdplas.f90
@@ -0,0 +1,22 @@
+      program tdplas
+      use readio  
+      use readio_medium
+      use BEM_medium
+      implicit none
+      integer :: st,current,rate
+!
+!     read in the input parameter for the present evolution
+      call system_clock(st,rate)
+      call read_medium_tdplas
+      call system_clock(current)
+      write(6,'("Done reading input, took", &
+            F10.3,"s")') real(current-st)/real(rate)
+!
+!     diagonalise matrix
+      call init_BEM
+      call system_clock(current)
+      write(6,'("Done , total elapsed time", &
+            F10.3,"s")') real(current-st)/real(rate)
+!         
+      stop
+      end

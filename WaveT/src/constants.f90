252f4e021d1c1102c714bb31ac5bd2fd297076e6
coccia <emanuele.coccia82@gmail.com>
2017-11-16 09:25:21 -0300

    Exact propagation also for SSE
    Added analytical propagation for energy term, also for SSE. Improved numerical stability for large number of vibrational states.

--- WaveT/src/constants.f90
+++ WaveT/src/constants.f90
@@ -5,7 +5,7 @@
       INTEGER, PARAMETER :: i4b = selected_int_kind(9)
       INTEGER, PARAMETER :: cmp = dbl
       INTEGER, PARAMETER :: flg = 10 
-      INTEGER, PARAMETER :: nvibmax = 15
+      INTEGER, PARAMETER :: nvibmax = 1000
 !
       ! constants, conversion factors, and number literals
       real(dbl), parameter :: pi=3.141592653589793D+00
@@ -13,7 +13,7 @@
       real(dbl), parameter :: debye_to_au=0.393456
       real(dbl), parameter :: TOANGS=0.52917724924D+00
       real(dbl), parameter :: ANTOAU=1.0D+00/TOANGS
-      real(dbl), parameter :: c=1.37036d2
+      real(dbl), parameter :: clight=1.37036d2
       real(dbl), parameter :: au_to_cm=219474.63137
       real(dbl), parameter :: cm_to_au=1.d0/au_to_cm
       real(dbl), parameter :: zero=0.d0
